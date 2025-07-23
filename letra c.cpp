b12bbadc25a3bd75dfca1f24072af98156696a48
Heloisa Mendes InfoB <megalolo19@gmail.com>
2025-07-23 07:59:16 -0300

    Criar letra c.cpp

---
+++ letra c.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+ #include <stdlib.h>           
+int main() {
+      printf("escreva o primeiro numero: ");
+      scantf("%d" &num1);
+      printf("escreva o segundo numero: ");
+      scantf("%d" &num2);
+}
