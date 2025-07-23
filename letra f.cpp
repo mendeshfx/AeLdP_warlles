3917509d6e24eb35f96b15a0cd28d028d6e073aa
Heloisa Mendes InfoB <megalolo19@gmail.com>
2025-07-23 08:03:17 -0300

    Create letra f.cpp

---
+++ letra f.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+ #include <stdlib.h>           
+  int main(){
+    printf("Informe o valor do raio: ");
+    scantf("%d" &raio);
+    printf("Informe o valor da altura: ");
+    scantf("%d" &altura);
+  }
